fbf261b4369650952bff0cee060be96178ff0782
monpeco <monpeco@gmail.com>
2019-05-14 18:21:22 -0400

    Add 'nombre' to the component

--- src/components/country.jsx
+++ src/components/country.jsx
@@ -4,14 +4,17 @@ class Country extends React.Component {
     state = {
         name: 'generic name',
         code: '0',
+        nombre: 'nombre cargando',
     }
     componentDidMount(){
         fetch('https://restcountries.eu/rest/v2/name/belgium')
         .then(resp => resp.json())
         .then( result => {
+            console.log(result)
             this.setState({
                 name: result[0].name,
                 code: result[0].callingCodes,
+                nombre: result[0].translations.es,
             });
         },(error) => {
             this.setState({name: 'Something went wrong'})
@@ -22,6 +25,7 @@ class Country extends React.Component {
         return (
             <div>
                 <h1>{this.state.name}</h1>
+                <h1>{this.state.nombre}</h1>
                 <p>The phone code is {this.state.code}</p>
             </div>
         );
