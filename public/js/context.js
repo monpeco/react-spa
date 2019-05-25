a189285ccb8f3c65829a5eea2a53bdf66873f212
monpeco <monpeco@gmail.com>
2019-05-25 07:45:01 -0400

    Add context example (with one single context)

---
+++ public/js/context.js
@@ -0,0 +1,28 @@
+function createFunction(){
+    var arr = [];
+    var number = 1;
+
+    arr.push( function(){
+        console.log(number);
+    } );
+
+    number = 2;
+    
+    arr.push( function(){
+        console.log(number);
+    } );
+    number = 3;
+
+    arr.push( function(){
+        console.log(number);
+    } );
+
+
+    return arr;
+}
+
+var functions = createFunction();
+
+functions[0]();
+functions[1]();
+functions[2]();
