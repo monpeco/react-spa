f2cbc3dc25a1cfaa4054a451a507fe0b0adbf4a7
monpeco <monpeco@gmail.com>
2019-05-25 07:52:56 -0400

    Add context example (with several nested context)

--- public/js/context.js
+++ public/js/context.js
@@ -2,25 +2,13 @@ function createFunction(){
     var arr = [];
     var number = 1;
 
-    arr.push( 
-        (function(number){
-            return function(){
-                console.log(number);
-            }
-        })(number) // create a new context
-    );
-
-    number = 2;
-    
-    arr.push( function(){
-        console.log(number);
-    } );
-    number = 3;
-
-    arr.push( function(){
-        console.log(number);
-    } );
-
+    for(var num=0; num < 5; num++ ){
+        arr.push(
+            (function(number){
+                return(function(){ console.log(number) })
+            })(num)
+        );
+    }
 
     return arr;
 }
@@ -30,3 +18,5 @@ var functions = createFunction();
 functions[0]();
 functions[1]();
 functions[2]();
+functions[3]();
+functions[4]();
